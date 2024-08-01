47679c5ada84583f3011ad0e0461795a666267a1
HinnMachache <bennyhinn253.bh@gmail.com>
2024-08-01 14:16:50 -0700

    Functions and Variabe type annotation

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,11 @@
+#!/usr/bin python3
+"""type-annotated function to_kv that takes a string k
+and an int OR float v as arguments and returns a tuple.
+"""
+from typing import Tuple, Union
+
+def to_kv(k: str, v: Union[int, float]) -> tuple(str, float):
+    """
+    Returns a tuple of str and squared int
+    """
+    return k, float(v) ** 2
