830dcbfead1b42fe498eafa4a84dd408bcacc84a
HinnMachache <bennyhinn253.bh@gmail.com>
2024-08-01 14:24:16 -0700

    Functions and Variabe type annotation

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -4,7 +4,8 @@ and an int OR float v as arguments and returns a tuple.
 """
 from typing import Tuple, Union
 
-def to_kv(k: str, v: Union[int, float]) -> tuple(str, float):
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
     Returns a tuple of str and squared int
     """
