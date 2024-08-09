fe665c59b1f49fdaaf3cf8c1e5eb0375d5eab734
HinnMachache <bennyhinn253.bh@gmail.com>
2024-08-09 22:15:50 +0000

    Async Python Functions

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -12,7 +12,9 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
 def measure_time(n: int, max_delay: int) -> float:
-    # Function to measure time
+    """
+    Function to measure time
+    """
     time_0: float = time.time()
     asyncio.run(wait_n(n, max_delay))
     time_1: float = time.time()
